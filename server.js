40d367f79f1866b360610d25a535aa66f4fa00fa
Brian T. <bptiburcio@gmail.com>
2024-07-01 17:21:43 -0400

    Add delete route for deleting notes

--- server.js
+++ server.js
@@ -1,22 +1,58 @@
-const { writeFile } = require('fs');
+const { writeFileSync } = require('fs');
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
 
 app.get("/", (req, res) => 
-    res.sendFile('./public/index.html')
+    res.sendFile(path.join(__dirname + '/public/index.html'))
 );
 
 app.get("/notes", (req, res) => 
-    res.sendFile('./public/notes.html')
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
+    // Read notes from the json file
     const notes = require('./db/db.json');
-    res.status(200).json(JSON.stringify(notes));
+    res.send(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    // Get the title and text from the request body
+    const { title, text } = req.body;
+
+    // Validate request body
+    if (!title || !text) {
+        return res.status(400).send('Invalid request body');
+    }
+
+    // Read notes from the json file
+    const notes = require('./db/db.json');
+    // Add the new note to the notes array
+    notes.push({ title, text, id });
+    // Write the notes array back to the json file
+    // Now with the new note included
+    writeFileSync('./db/db.json', JSON.stringify(notes, null, 2));
+    
+    // Send the new note back to the client
+    res.json({ title, text });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    // Get the id of the note to be deleted
+    const id = req.params.id;
+
+    // Read notes from the json file
+    const notes = require('./db/db.json');
+    // Remove the note with the given id
+    const newNotes = notes.filter((note, index) => index != id);
+    // Write the new notes array back to the json file
+    writeFileSync('./db/db.json', JSON.stringify(newNotes, null, 2));
+    // Send the deleted note back to the client
+    res.json({ id });
 });
 
 app.listen(3001, () => {
